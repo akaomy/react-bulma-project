ffbed3f1e419f6aa4a4b3fbea32a4e10cfcf5c5d
akozlova <annakozlova92@gmail.com>
2020-09-09 12:46:26 -0700

    wip

--- src/components/Counter/StepConfig.js
+++ src/components/Counter/StepConfig.js
@@ -7,18 +7,19 @@ function StepConfig(props) {
             <label> Enter step: </label>
             <input
                 value={props.step}
-                onChange={(e)=> {
-                        const errMsg = document.getElementById('error-msg')
-                        const red = errMsg.style.color = 'red';
-                        let userInput = e.target.value;
+                // onChange={(e)=> {
+                //         const errMsg = document.getElementById('error-msg')
+                //         const red = errMsg.style.color = 'red';
+                //         let userInput = e.target.value;
 
-                        if (typeof userInput === 'string' || userInput instanceof String)
-                            errMsg.innerText = 'ONLY NUMBERS ARE ALLOWED';
-                        else {
-                            props.onStepChange(+userInput);
-                        }
-                    }
-                } />
+                //         if (typeof userInput === 'string' || userInput instanceof String)
+                //             errMsg.innerText = 'ONLY NUMBERS ARE ALLOWED';
+                //         else {
+                //             props.onStepChange(+userInput);
+                //         }
+                //     }
+                // } 
+                />
         </div>
     )
 }
