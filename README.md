fbff2f28785bb35e2c52db5c05acc0d196a0e181
akozlova <annakozlova92@gmail.com>
2020-11-09 14:44:36 -0800

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-This project is created as a playground for small subprojects that are built on React
-
-## To run
-
-In the project directory run:
-
-### `npm start`
---
+++ precache-manifest.aeccbdccfbaadca1bfb2b981468f9e32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e86f7a1a516a118818176d95773e5a3a",
+    "url": "/react-bulma-project/index.html"
+  },
+  {
+    "revision": "7edd28864d9f78da6c50",
+    "url": "/react-bulma-project/static/css/2.920bdfdc.chunk.css"
+  },
+  {
+    "revision": "bda216b7a2865e2b60c2",
+    "url": "/react-bulma-project/static/css/main.56490fd6.chunk.css"
+  },
+  {
+    "revision": "7edd28864d9f78da6c50",
+    "url": "/react-bulma-project/static/js/2.4c585d3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-bulma-project/static/js/2.4c585d3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bda216b7a2865e2b60c2",
+    "url": "/react-bulma-project/static/js/main.4b0db014.chunk.js"
+  },
+  {
+    "revision": "e3dd2f2f9e11ddf4718d",
+    "url": "/react-bulma-project/static/js/runtime-main.f9a384a3.js"
+  }
+]);
