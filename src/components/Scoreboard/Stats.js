6d8f3c52961b76740e2f47b75380fbb7ca9c9f88
akozlova <annakozlova92@gmail.com>
2020-11-13 21:36:28 -0800

    add total score in stats component

--- src/components/Scoreboard/Stats.js
+++ src/components/Scoreboard/Stats.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 export default function Stats(props) {
+    console.log(props)
+    const totalScore = props.players.reduce((accumulator, player) => {
+        return accumulator + player.score
+    }, 0)
     return (
         <div className="stats">
             <p>Players:{props.players.length}</p>
-            <p>Total Points:0</p> {/*props.players.score*/}
+            <p>Total Points: {totalScore}</p> {/*props.players.score*/}
         </div>
     );
 }
